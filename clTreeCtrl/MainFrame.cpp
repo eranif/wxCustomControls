d5ac8e8ac2991272b8a17d9e8096f5a3faef9aa1
Eran Ifrah <eran.ifrah@gmail.com>
2018-09-04 20:03:06 +0300

    .

--- clTreeCtrl/MainFrame.cpp
+++ clTreeCtrl/MainFrame.cpp
@@ -186,6 +186,7 @@ void MainFrame::OnNextSibling(wxCommandEvent& event)
     m_tree->SelectItem(m_tree->GetNextSibling(m_tree->GetFocusedItem()));
     m_tree->EnsureVisible(m_tree->GetFocusedItem());
 }
+
 void MainFrame::OnPrevSibling(wxCommandEvent& event)
 {
     m_tree->SelectItem(m_tree->GetPrevSibling(m_tree->GetFocusedItem()));
